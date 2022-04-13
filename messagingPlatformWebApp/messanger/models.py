9e174e4b194f107a28ca6643d29ad92e8fd8c630
degradedlocket <conorm456@hotmail.com>
2022-04-13 21:57:53 +0100

    Messgaes displaying

--- messagingPlatformWebApp/messanger/models.py
+++ messagingPlatformWebApp/messanger/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Message(models.Model):
+    def __str__(self):
+        return self.msgText
+    msgText = models.CharField(max_length=240)
+    msgDate = models.DateTimeField("date sent")
+    userFrom = models.CharField(max_length=5)
--- messagingPlatformWebApp/messanger/views.py
+++ messagingPlatformWebApp/messanger/views.py
@@ -1,7 +1,15 @@
 
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Message
+
 # Create your views here.
 
 def index(request):
-    context ={"msg":"Hello There"}
+    msgs = Message.objects.order_by("msgDate")
+    context ={"msgs":msgs}
+
     return render(request, "messanger.html", context)
+
+#def m(request):
+#    msgs = M
+#    return render(request, "messanger.html", {"msg":msg})
